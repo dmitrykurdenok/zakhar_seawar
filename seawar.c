85d4bf57de35e95d8c38e4d4d4f326c61f392714
dmitry-laptop\Дмитрий <kurdenok@ngs.ru>
2019-01-13 10:42:24 +0700

    Объявлены и инициализированы игровые карты; добавлена функция printMaps()

---
+++ seawar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define M 10
+#define N 10
+
+char map_comp[M][N + 1];
+char map_user[M][N + 1];
+
+void printMaps()
+{
+    system("cls");
+    printf("  computer      user   \n");
+    printf(" ABCDEFGHIJ  ABCDEFGHIJ\n");
+    for (int i = 0; i < M; ++i)
+        printf("%d%s %d%s\n", i, map_comp[i], i, map_user[i]);
+}
+
+int main()
+{
+    for (int i = 0; i < M; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            map_comp[i][j] = ' ';
+            map_user[i][j] = ' ';
+        }
+        map_comp[i][N] = '\0';
+        map_user[i][N] = '\0';
+    }
+    printMaps();
+    return 0;
+}
